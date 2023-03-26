e3b631ef9a6bb35b87a7bcc3cdaa5cdac02ee3d4
ollven <ollven@yandex.ru>
2023-03-26 16:55:08 +0200

    Add some dummy change

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,7 +10,7 @@ class MainTest {
         int b = 5;
         int c = 7;
         int actual = Main.result(a, b, c);
-        int expected = 10;
+        int expected = 11;
         Assertions.assertEquals(expected, actual);
     }
 }
