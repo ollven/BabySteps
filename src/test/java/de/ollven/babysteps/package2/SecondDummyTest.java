797d4bc3b2699345673865e56de9f83064cc2b2d
Olga Sventukh <ollven@yandex.ru>
2023-06-08 13:06:39 +0200

    Update SecondDummyTest.java

--- src/test/java/de/ollven/babysteps/package2/SecondDummyTest.java
+++ src/test/java/de/ollven/babysteps/package2/SecondDummyTest.java
@@ -26,7 +26,7 @@ public class SecondDummyTest {
 
     @Test
     public void secondtestThatIsSlowButAlwaysPasses() throws InterruptedException {
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         Assertions.assertTrue(true);
     }
 }
