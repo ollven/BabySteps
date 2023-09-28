6c54b1cecd10c0c4c43db3fc9347d69755227ebc
Olga Sventukh Second <olga.sventukh@jetbrains.com>
2023-09-28 13:42:08 +0000

    Commit message #CR3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ public class Main {
             System.out.print(sum + "CSPcheck333");
             System.out.print(sum + "TW-70782");
             System.out.print(sum + "TTW-83609");
-          	System.out.print(sum + "CodeReview");
+          	System.out.print(sum + "CodeReview1");
 
         }
     }
