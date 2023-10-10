3c97857dafa484d865186ee2b069bfdad796c5f1
ollven <ollven@yandex.ru>
2023-10-10 16:18:00 +0200

    Change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
             System.out.print(sum + " ");
             System.out.print(sum + "CSPcheck333");
             System.out.print(sum + "TW-70782");
-            System.out.print(sum + "TTW-83609");
+            System.out.print(sum + "Change was made");
 
 
         }
