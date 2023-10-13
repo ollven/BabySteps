85f49a1f21e508e80e60dd20e587f4266496dd12
Olga Sventukh Second <olga.sventukh@jetbrains.com>
2023-10-13 11:39:50 +0000

    commit
    🍒 Cherry-picked from review MAIN-CR-7
    
    Co-authored-by: ollven <ollven@yandex.ru>
    
    
    Merge-request: MAIN-CR-8
    Merged-by: Olga Sventukh Second <olga.sventukh@jetbrains.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,9 +16,8 @@ public class Main {
             System.out.print(sum + " ");
             System.out.print(sum + "CSPcheck333");
             System.out.print(sum + "TW-70782");
-            System.out.print(sum + "Change was made");
-
-
+            System.out.print(sum + "TTW-83609");
+          	System.out.print(sum + "CodeReview34566juhlhljhhj");
         }
     }
 
