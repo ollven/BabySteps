eef2e3df926b42e14272e529826e78bd52e17f89
ollven <ollven@TeamCity>
2024-01-15 16:26:33 +0100

    TeamCity change in 'BabySteps' project: SonarQube Server 'sonar' was added

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -43,6 +43,15 @@ project {
             clientSecret = "credentialsJSON:34618669-c7e5-4214-84c0-5b5dc2e001cf"
             serviceMessageMaxNotificationsPerBuild = "-1"
         }
+        feature {
+            id = "PROJECT_EXT_50"
+            type = "sonar-qube"
+            param("useToken", "true")
+            param("name", "sonar")
+            param("id", "d84ee010-c3e1-488f-9f29-60a9af1b11ad")
+            param("url", "http://localhost:9000")
+            param("token", "scrambled:c3FhXzc1ZDE2YmI4YTEyMjk2ODk5ZTkwMjJiYTAzMmQ1MjBiMWMwOGUwOWI=")
+        }
     }
 
     cleanup {
