fadf15f5e622e5b92a18bd4f2722d9f6755a2fe8
ollven <ollven@TeamCity>
2024-02-13 15:49:10 +0100

    TeamCity change in 'BabySteps' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -89,7 +89,6 @@ object Build : BuildType({
         maven {
             goals = "clean package"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
-            jdkHome = "%env.JDK_11%"
             param("teamcity.coverage.idea.includePatterns", "de.ollven.*")
             param("teamcity.coverage.jacoco.classpath", "+:build/main/**/*")
         }
