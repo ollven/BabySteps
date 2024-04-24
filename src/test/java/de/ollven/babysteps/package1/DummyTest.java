9a6110e950dc5642697742428b10d77157e673d7
Olga Sventukh <ollven@yandex.ru>
2024-04-24 13:32:53 +0200

    Update DummyTest.java

--- src/test/java/de/ollven/babysteps/package1/DummyTest.java
+++ src/test/java/de/ollven/babysteps/package1/DummyTest.java
@@ -12,13 +12,13 @@ public class DummyTest {
 
     @Test
     public void testThatAlwaysPasses() throws InterruptedException {
-        Thread.sleep(20000);
+        Thread.sleep(200000);
         Assertions.assertTrue(true);
     }
 
     @Test
     public void testThatIsSlowButAlwaysFails() throws InterruptedException {
-        Thread.sleep(20000);
+        Thread.sleep(200000);
         Assertions.assertTrue(true);
     }
 
