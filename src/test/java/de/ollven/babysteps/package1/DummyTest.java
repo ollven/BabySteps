183a20df58dcc6bdfb50a7e8cc4b28629c36459a
Olga Sventukh <olga.sventukh@jetbrains.com>
2024-07-09 10:39:40 +0000

    VCS label

--- src/test/java/de/ollven/babysteps/package1/DummyTest.java
+++ src/test/java/de/ollven/babysteps/package1/DummyTest.java
@@ -7,24 +7,24 @@ public class DummyTest {
 
     @Test
     public void testThatAlwaysFails() throws InterruptedException {
-        Thread.sleep(2000);
+        Thread.sleep(500);
         Assertions.assertTrue(false);    }
 
     @Test
     public void testThatAlwaysPasses() throws InterruptedException {
-        Thread.sleep(1000);
+        Thread.sleep(500);
         Assertions.assertTrue(true);
     }
 
     @Test
     public void testThatIsSlowButAlwaysFails() throws InterruptedException {
-        Thread.sleep(1000);
+        Thread.sleep(500);
         Assertions.assertTrue(true);
     }
 
     @Test
     public void testThatIsSlowButAlwaysPasses() throws InterruptedException {
-        Thread.sleep(1000);
+        Thread.sleep(500);
         Assertions.assertTrue(true);
     }
 }
