19493d20d125a7d43a423363bab3e87189540fd1
Olga Sventukh <ollven@yandex.ru>
2024-08-01 12:02:40 +0200

    commit1 - make one test fail

--- src/test/java/de/ollven/babysteps/package1/DummyTest.java
+++ src/test/java/de/ollven/babysteps/package1/DummyTest.java
@@ -8,7 +8,7 @@ public class DummyTest {
     @Test
     public void testThatAlwaysFails() throws InterruptedException {
         Thread.sleep(1000);
-        Assertions.assertTrue(true);    }
+        Assertions.assertTrue(false);    }
 
     @Test
     public void testThatAlwaysPasses() throws InterruptedException {
