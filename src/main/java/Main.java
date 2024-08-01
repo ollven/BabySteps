52c1d2448e024003779ae6c016694e09d3d443ca
Olga Sventukh <ollven@yandex.ru>
2024-08-01 16:23:45 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,4 +39,4 @@ public class Main {
 
 }
 ///blwsdqwdefeferуацуацdsffxaxwqwqecwecwecwce
-///dcqwececervervdfsadfasdvferggerggegrewrgdcwefwefeqwfwqd    dwedwefewq dvdfvdsvds regergregre rferegerwg. dsfsfvfdsdvf
+///dcqwececervervdfsadfasdvferggerggegrew
