0ad57d3d6272ad777b6b99745cb606458511c629
Olga Sventukh <ollven@yandex.ru>
2024-08-26 16:12:27 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,8 +14,7 @@ public class Main {
             int c = toint.nextInt();
             int sum = result(a, b, c);
             System.out.print(sum + " ");
-            System.out.print(sum + "CSPcheck333");
-
+           
         }
     }
 
