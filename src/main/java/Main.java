ebe27f19cb8187a08b35700715203cde44fcd8eb
Olga Sventukh <ollven@yandex.ru>
2024-08-27 13:46:30 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,5 +28,5 @@ public class Main {
         return sum;
     }
 
-    /////djkjvjkvbjvjvbrxhhfhfhfedewfweffgergfergergergregdcdcdscasewewrgerrg
+    /////djkjvjkvbjvjvbrxhhfhfhfedewfweffgergfergergergregdcdcdscasewewrgerrgedfwefwefweff
 }
